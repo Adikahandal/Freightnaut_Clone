3dbcd2ba9d08d79ee1a346190ab30f9767d82a06
Aditya Kahandal <adityakahandal1308@gmail.com>
2025-02-24 10:26:55 +0530

    f

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import About from './components/Home_page/About';
 import Body from './components/Home_page/Body';
 import FFS from './components/Home_page/FFS';
-import Form from './components/Home_page/Form';
 import Freightnaut_soft from './components/Home_page/Freightnaut_soft';
 import WhyUs from './components/Home_page/why_us';
 
@@ -14,7 +13,6 @@ function App() {
       <Freightnaut_soft/>
       <About/>
       <WhyUs/>
-      <Form/>
     </>
   );
 }
